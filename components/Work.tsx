67dbb7f40334fc35e7af720496debb473970fecf
Mouhib <mouhibenns@gmail.com>
2024-01-29 00:18:14 +0100

    Added links to projects

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Work() {
   return (
@@ -6,7 +7,7 @@ export default function Work() {
       <h1 className="text-4xl font-bold text-center">My latest work</h1>
       <div className="flex flex-col space-y-24">
         <div className="grid grid-cols-1 md:grid-cols-8 gap-x-6">
-          <div className="col-span-3 py-6 space-y-4">
+          <div className="col-span-5 md:col-span-3 py-6 space-y-4">
             <h1 className="text-2xl font-bold">BagageVoyage</h1>
             <p className="text-lg">
               A project in collaboration with x where they are responsible for
@@ -27,9 +28,29 @@ export default function Work() {
               <li>Development (Frontend and backend)</li>
               <li>Hosting and monitoring</li>
             </ul>
-            <p className="text-lg">
-              👇 click here to check the website yourself
-            </p>
+            <div className="flex flex-col space-y-6">
+              <p className="text-lg">
+                👇 click here to check the website yourself
+              </p>
+              <Link href="https://bagagevoyage.com">
+                <div className="flex w-ful py-3 rounded-lg space-x-4 items-center">
+                  <div className="aspect-square h-16 w-16 bg-gray-300 rounded-lg relative">
+                    <Image
+                      className="rounded-lg object-cover object-center"
+                      src="/bagagevoyage-logo.jpg"
+                      alt="bagagevoyage.com logo"
+                      fill
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1">
+                    <h1 className="font-bold">BagageVoyage</h1>
+                    <p className="text-gray-600 text-sm">
+                      Transportation services between Tunisia & Europe
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
           <div className="col-span-5 w-full aspect-square grid grid-cols-2 gap-4">
             <div className="rounded-lg bg-gray-200 col-span-2 border-2 border-gray-200 relative">
@@ -60,7 +81,7 @@ export default function Work() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-8 gap-x-6">
-          <div className="col-span-3 py-6 space-y-4">
+          <div className="col-span-5 md:col-span-3 py-6 space-y-4">
             <h1 className="text-2xl font-bold">Eatourist</h1>
             <p className="text-lg">
               A side project of a web application that helps restaurants bring
@@ -73,9 +94,29 @@ export default function Work() {
               restaurant and see advanced analytics like the number of visitors,
               number of orders and revenue that came from the app.
             </p>
-            <p className="text-lg">
-              👇 click here to check the website yourself
-            </p>
+            <div className="flex flex-col space-y-6">
+              <p className="text-lg">
+                👇 click here to check the website yourself
+              </p>
+              <Link href="https://eatourist.com">
+                <div className="flex w-ful py-3 rounded-lg space-x-4 items-center">
+                  <div className="aspect-square h-16 w-16 bg-gray-300 rounded-lg relative">
+                    <Image
+                      className="rounded-lg object-cover object-center"
+                      src="/eatourist-logo.png"
+                      alt="bagagevoyage.com logo"
+                      fill
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1">
+                    <h1 className="font-bold">Eatourist</h1>
+                    <p className="text-gray-600 text-sm">
+                      Menu digitization and translation for restaurants
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
           <div className="col-span-5 w-full aspect-square grid grid-cols-8 grid-rows-2 gap-4">
             <div className="rounded-lg bg-gray-200 col-span-4 border-gray-200 border-2 relative">
